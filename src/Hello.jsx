48c5610003640e1a7ce2310af5c5d78f53fe9304
Baejb <bjb6478@naver.com>
2022-07-01 06:24:58 +0900

    객체로 만들어서 style 적용

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 function Hello(){
-    return <div style={{ marginTop: '10px', backgroundColor: 'red'}}>Hello World!</div>;
+    const PracticeStyle ={
+        marginTop: '10px',
+        backgroundColor: 'blue',
+    };
+    return( <>
+        <div style={PracticeStyle}>Hello World!</div>
+        <div style={PracticeStyle}>Hello World!</div>
+        <div style={PracticeStyle}>Hello World!</div>
+        </> 
+    );
+
 }
 export default Hello;
